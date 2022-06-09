17b978a5a2b5c56e87b24af95f45eb0e63627db7
Allen <valerio.allen24@gmail.com>
2022-06-09 23:25:57 +1200

    Alarm
    Needs testing

--- Assets/John/Scripts/SFX/AlarmSFX.cs
+++ Assets/John/Scripts/SFX/AlarmSFX.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class AlarmSFX : MonoBehaviour
 {
+    // spawn this when the a machine gets broken
+    // assign the audio clip here
+    // you can choose from the audio folder
     AudioSource _alarm;
     private void Start()
     {
         _alarm = GetComponent<AudioSource>();
     }
-
+    public void AlarmOn()
+    {
+        _alarm.Play();
+    }
 }
